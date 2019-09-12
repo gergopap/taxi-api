14ec10a7c2b7dcd731c5cde64e5e560b072b3e83
Gergely Pap <pap133@gmail.com>
2019-09-12 22:23:07 +0200

    update kong config

--- kongconfig.sh
+++ kongconfig.sh
@@ -1,58 +1,58 @@
 curl -i -X POST \
-  --url http://ec2-18-220-209-34.us-east-2.compute.amazonaws.com:8001/services/ \
+  --url http://ec2-18-191-110-235.us-east-2.compute.amazonaws.com:8001/services/ \
   -d 'name=taxiapi' \
-  -d 'url=http://ec2-18-220-209-34.us-east-2.compute.amazonaws.com'
+  -d 'url=http://ec2-18-191-110-235.us-east-2.compute.amazonaws.com'
 
 curl -X POST \
-  --url http://ec2-18-220-209-34.us-east-2.compute.amazonaws.com:8001/services/taxiapi/routes \
+  --url http://ec2-18-191-110-235.us-east-2.compute.amazonaws.com:8001/services/taxiapi/routes \
   -H 'Content-Type: application/x-www-form-urlencoded' \
-  -d 'hosts[]=18.220.209.34'
+  -d 'hosts[]=ec2-18-191-110-235.us-east-2.compute.amazonaws.com'
 
 curl -X POST \
-  --url http://ec2-18-220-209-34.us-east-2.compute.amazonaws.com:8001/plugins \
+  --url http://ec2-18-191-110-235.us-east-2.compute.amazonaws.com:8001/plugins \
   -d "name=rate-limiting"  \
   -d "config.second=5" \
   -d "config.hour=10000"
 
 curl -i -X POST \
-  --url http://ec2-18-220-209-34.us-east-2.compute.amazonaws.com:8001/upstreams \
+  --url http://ec2-18-191-110-235.us-east-2.compute.amazonaws.com:8001/upstreams \
   -H 'Content-Type: application/x-www-form-urlencoded' \
   -d 'name=taxiapi'
 
 curl -i -X POST \
-  --url http://ec2-18-220-209-34.us-east-2.compute.amazonaws.com:8001/upstreams/taxiapi/targets \
+  --url http://ec2-18-191-110-235.us-east-2.compute.amazonaws.com:8001/upstreams/taxiapi/targets \
   -H 'Content-Type: application/x-www-form-urlencoded' \
-  -d 'target=taxi-api-1:10010'
+  -d 'target=taxi-api-1:8000'
   -d 'weight=50'
 
 curl -i -X POST \
-  --url http://ec2-18-220-209-34.us-east-2.compute.amazonaws.com:8001/upstreams/taxiapi/targets \
+  --url http://ec2-18-191-110-235.us-east-2.compute.amazonaws.com:8001/upstreams/taxiapi/targets \
   -H 'Content-Type: application/x-www-form-urlencoded' \
-  -d 'target=taxi-api-2:10010'
+  -d 'target=taxi-api-2:8000'
   -d 'weight=50'
 
 curl -i -X POST \
-  --url http://ec2-18-220-209-34.us-east-2.compute.amazonaws.com:8001/consumers/ \
+  --url http://ec2-18-191-110-235.us-east-2.compute.amazonaws.com:8001/consumers/ \
   -d "username=mobile"
 
 curl -i -X POST \
-  --url http://ec2-18-220-209-34.us-east-2.compute.amazonaws.com:8001/consumers/ \
+  --url http://ec2-18-191-110-235.us-east-2.compute.amazonaws.com:8001/consumers/ \
   -d "username=web"
 
 curl -i -X POST \
-  --url http://ec2-18-220-209-34.us-east-2.compute.amazonaws.com:8001/consumers/mobile/plugins \
+  --url http://ec2-18-191-110-235.us-east-2.compute.amazonaws.com:8001/consumers/mobile/plugins \
   -d "name=rate-limiting" \
   -d "config.minute=5"
 
 curl -i -X 
-  POST http://ec2-18-220-209-34.us-east-2.compute.amazonaws.com:8001/consumers/web/plugins \
+  POST http://ec2-18-191-110-235.us-east-2.compute.amazonaws.com:8001/consumers/web/plugins \
   -d "name=rate-limiting" \
   -d "config.minute=5"
 
 curl -i -X POST \
-  --url http://ec2-18-220-209-34.us-east-2.compute.amazonaws.com:8001/consumers/web/key-auth/ \
+  --url http://ec2-18-191-110-235.us-east-2.compute.amazonaws.com:8001/consumers/web/key-auth/ \
   -d 'key=web'
 
 curl -i -X POST \
-  --url http://ec2-18-220-209-34.us-east-2.compute.amazonaws.com:8001/consumers/mobile/key-auth/ \
+  --url http://ec2-18-191-110-235.us-east-2.compute.amazonaws.com:8001/consumers/mobile/key-auth/ \
   -d 'key=mobile'
